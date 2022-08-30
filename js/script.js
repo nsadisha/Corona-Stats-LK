1a3d8d0de0ad803d16ec662e0c584a3179da9aa3
Sadisha Nimsara <nsadisha@gmail.com>
2022-08-31 03:40:48 +0530

    mobile scrolling issue fixed with navbar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const navBar = document.querySelector('.navbar')
 window.addEventListener("scroll", e => {
     if(window.scrollY <= 20){
         navBar.classList.remove('navbar-scroll')
+        navBar.classList.add('navbar-dark')
     }else{
         navBar.classList.add('navbar-scroll')
+        navBar.classList.remove('navbar-dark')
     }
+})
+
+//mobile collaps issue
+const links = document.querySelectorAll('.navbar .nav-link')
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        document.querySelector('.navbar-toggler').click()
+    })
 })
